4a18a1394a3fafdd3fff4110b03916ac513e6478
Ian Young <ian.greenleaf@gmail.com>
2011-05-17 00:05:04 -0700

    Sad lonely browsers who can't read HTML5

--- application.js
+++ application.js
@@ -1,6 +1,25 @@
 $(function() {
-  $("#button").click(function() {
-    $("audio#bomb").get(0).play();
-  });
-  $("audio#bomb").attr("controls", false);
+  if (!!(document.createElement('audio').canPlayType)) {
+    $("#button").click(function() {
+      $("audio#bomb").get(0).play();
+    });
+    $("audio#bomb").attr("controls", false);
+  } else {
+    // We only use jPlayer if lacking native support
+    $("#button").after($("<div id='jplayer'></div>"));
+    $("#jplayer").jPlayer({
+      ready: function() {
+        $(this).jPlayer("setMedia", {
+          mp3: $("audio#bomb source.mp3").attr("src"),
+          oga: $("audio#bomb source.ogg").attr("src")
+        });
+      },
+      swfPath: "jplayer",
+      supplied: "mp3, oga",
+      preload: "auto"
+    });
+    $("#button").click(function() {
+      $("#jplayer").jPlayer("play", 0);
+    });
+  }
 });
