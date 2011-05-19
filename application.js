8527828d80a5237dfc10058a24cd43a6b0715d38
Ian Young <ian.greenleaf@gmail.com>
2011-05-18 23:47:51 -0700

    Only load jPlayer library if needed

--- application.js
+++ application.js
@@ -6,20 +6,22 @@ $(function() {
     $("audio#bomb").attr("controls", false);
   } else {
     // We only use jPlayer if lacking native support
-    $("#button").after($("<div id='jplayer'></div>"));
-    $("#jplayer").jPlayer({
-      ready: function() {
-        $(this).jPlayer("setMedia", {
-          mp3: $("audio#bomb source.mp3").attr("src"),
-          oga: $("audio#bomb source.ogg").attr("src")
-        });
-      },
-      swfPath: "jplayer",
-      supplied: "mp3, oga",
-      preload: "auto"
-    });
-    $("#button").click(function() {
-      $("#jplayer").jPlayer("play", 0);
+    $.getScript("jplayer/jquery.jplayer.min.js", function() {
+      $("#button").after($("<div id='jplayer'></div>"));
+      $("#jplayer").jPlayer({
+        ready: function() {
+          $(this).jPlayer("setMedia", {
+            mp3: $("audio#bomb source.mp3").attr("src"),
+            oga: $("audio#bomb source.ogg").attr("src")
+          });
+        },
+        swfPath: "jplayer",
+        supplied: "mp3, oga",
+        preload: "auto"
+      });
+      $("#button").click(function() {
+        $("#jplayer").jPlayer("play", 0);
+      });
     });
   }
 });
